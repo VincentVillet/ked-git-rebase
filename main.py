8dde4a9ae06ee9bfd5021be810d1e81d55f8c591
Vincent Villet <vincent.villet@gmail.com>
2021-11-17 15:47:08 +0100

    [ADD] New operation in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def my_operation(input_nb):
-    return 2 * input_nb
+def my_new_operation(input_nb):
+    return 3 * input_nb
 
 
 if __name__ == "__main__":
     a = 2
-    print(my_operation(a))
+    print(my_new_operation(a))
